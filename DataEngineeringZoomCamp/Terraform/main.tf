924341acf3e420ef419219ec03822d744d8f746b
Samhitha Gundabathula <samhitha.gundabathula@gmail.com>
2026-01-21 13:57:36 -0400

    commit

---
+++ DataEngineeringZoomCamp/Terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+  credentials = "./Keys/my-creds.json"  #your service account key file
+  project = "western-tea-485016-j2"
+  region  = "us-central1"
+}
+
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraformpractisebucket"  #globally unique bucketname
+  location      = "US"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 1 // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
